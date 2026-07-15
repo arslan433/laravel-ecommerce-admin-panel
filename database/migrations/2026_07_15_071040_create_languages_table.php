5ab8eb2e0d98bbde439af5a504ac2981e2b5bbb9
Arslan <arslanpc65@gmail.com>
2026-07-15 12:18:21 +0500

    Languages Migration

---
+++ database/migrations/2026_07_15_071040_create_languages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('languages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('code')->nullable();
+            $table->string('directory')->nullable();
+            $table->integer('sort_order')->default(0);
+            $table->boolean('default')->default(false);
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('languages');
+    }
+};
